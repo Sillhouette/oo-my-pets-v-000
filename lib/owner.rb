fea8c97d4c00f260b2701f31052cddbf2be3649b
Richard Melchior <raustin.melchior@gmail.com>
2018-06-06 01:45:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
-  # code goes here
+  attr_accessor :name
+
+  @@all = [];
+
+  def initialize(name)
+
+  end
+
+  def self.all
+    @@all;
+  end
+
+  def self.count_owners
+    @@all.length;
+  end
+
+  def reset
+    self.all.clear;
+  end
 end
